62c096afc55747da5e1f4af18e2b8f715991cc76
Dustin Burns <dustinburns@gmail.com>
2018-01-09 11:37:39 -0800

    Develop (#3)
    * retreive the user information
    
    * testing
    
    * made the fixes suggested in meeting

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+// import ProfilePage from './components/ProfilePage.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      userProfile: []
     }
   }
 
   componentDidMount() {
+    this.findUser('dust_off', (user)=> {
+      this.setState({
+        userProfile: user
+      })
+    })
     $.ajax({
       url: '/items',
       success: (data) => {
@@ -25,9 +32,21 @@ class App extends React.Component {
     });
   }
 
+  findUser(user, cb) {
+    $.ajax({
+      url: `/user/${user}`,
+      success: (data) => {
+        cb(data)
+      },
+      error: (err) => {
+        console.log('err', err);
+      }
+    });
+  }
+
   render () {
     return (<div>
-      <h1>Item List</h1>
+      <h1>Item List: {this.state.userProfile.name}</h1>
       <List items={this.state.items}/>
     </div>)
   }
