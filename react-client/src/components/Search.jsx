4a54abaf2adb2431d724a05e8b7c452ab5f59d8a
misha <mishall.afzal@gmail.com>
2018-01-09 14:59:42 -0800

    connected search to fetch

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -19,7 +19,6 @@ class Search extends React.Component {
   handleSubmit() {
     console.log('clicked', this.state.query);
     this.props.fetch('book', this.state.query);
-    // write ajax request here
   }
 
   render() {
