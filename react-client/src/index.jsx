f1dc3a36e67507e9c5fb65494ca33cd5b15f9e02
misha <mishall.afzal@gmail.com>
2018-01-09 14:58:48 -0800

    connected search to fetch

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -5,7 +5,7 @@ import List from './components/List.jsx';
 import ProfilePage from './components/ProfilePage.jsx'
 import BookPage from './components/BookPage.jsx'
 import HomePage from './components/HomePage.jsx'
-import Search from './components/Search.jsxgt'
+import Search from './components/Search.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -32,6 +32,7 @@ class App extends React.Component {
   }
 
   fetch(thing, id, cb) {
+    console.log('FETCH');
     $.ajax({
       url: `/${thing}/${id}`,
       success: (data) => {
@@ -57,7 +58,7 @@ class App extends React.Component {
   render () {
     return (
     <div>
-    <Search />
+    <Search fetch={this.fetch.bind(this)} />
 
       <div className="selections">
         Test Pages:
