c52aa38d45464aacc8b3c1d75307b9bcf178e238
Stephan Lefort <lefortsge@gmail.com>
2018-01-11 19:18:32 -0800

    clean code and Implement handleUserclick

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import $ from 'jquery';
+import StarRatings from 'react-star-ratings';
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      bestSellerBooks: [],
       bestSellerBook1: '',
       bestSellerBook2: '',
       bestSellerBook3: '',
@@ -13,9 +15,10 @@ class HomePage extends React.Component {
       imageUrl3: '',
       star1: 0,
       star2: 0,
-      star3: 0,
+      star3: 0
     };
     this.getBestSellersBooks = this.getBestSellersBooks.bind(this);
+    this.handleUserClick = this.handleUserClick.bind(this);
   }
 
   componentDidMount() {
@@ -28,7 +31,7 @@ class HomePage extends React.Component {
       type: 'GET',
     })
     .done((result) => {
-      const books = result.results;
+      const books = result.results.slice(0, 3); // use a slice over collection
       console.log(books);
       const book1 = books[0];
       const book2 = books[1];
@@ -36,6 +39,17 @@ class HomePage extends React.Component {
       const b1isbn = book1.isbns[0].isbn13;
       const b2isbn = book2.isbns[0].isbn13;
       const b3isbn = book3.isbns[0].isbn13;
+      books.map((book) => {
+
+          this.props.fetch('book', b1isbn, (book) => {
+            this.setState({
+            imageUrl1: book.imageURL,
+            star1: book.averageRating,
+          });
+        });
+
+      });
+
       if (b1isbn) {
         this.props.fetch('book', b1isbn, (book) => {
           this.setState({
@@ -63,6 +77,7 @@ class HomePage extends React.Component {
       }
 
       this.setState({
+        bestSellerBooks: books.slice(0, 3),
         bestSellerBook1: book1,
         bestSellerBook2: book2,
         bestSellerBook3: book3,
@@ -73,6 +88,10 @@ class HomePage extends React.Component {
     });
   }
 
+  handleUserClick () {
+    console.log("I was clicked");
+  }
+
   render() {
     return (
       <div>
@@ -81,15 +100,16 @@ class HomePage extends React.Component {
           {this.state.bestSellerBook1
             ?
               <div>
-                <img src={this.state.imageUrl1} />
-                <div className="bookTitle1"> {this.state.bestSellerBook1.title} star: {this.state.star1}</div>
+                <img src={this.state.imageUrl1} onClick={this.handleUserClick} />
+                <StarRatings rating={Number(this.state.star1)} isSelectable={false} isAggregateRating={true} numOfStars={5} />
+                <div className="bookTitle1"> {this.state.bestSellerBook1.title}</div>
                 <div className="bookTitle1"> by {this.state.bestSellerBook1.author} </div>
                 <div className="bookTitle1"> {this.state.bestSellerBook1.description} </div>
-                <img src={this.state.imageUrl2} />
+                <img src={this.state.imageUrl2} onClick={this.handleUserClick}/>
                 <div className="bookTitle2"> {this.state.bestSellerBook2.title} star: {this.state.star2}</div>
                 <div className="bookTitle2"> by {this.state.bestSellerBook2.author} </div>
                 <div className="bookTitle2"> {this.state.bestSellerBook2.description} </div>
-                <img src={this.state.imageUrl3} />
+                <img src={this.state.imageUrl3} onClick={this.handleUserClick}/>
                 <div className="bookTitle3"> {this.state.bestSellerBook3.title} star: {this.state.star3}</div>
                 <div className="bookTitle3"> by {this.state.bestSellerBook3.author} </div>
                 <div className="bookTitle3"> {this.state.bestSellerBook3.description} </div>
