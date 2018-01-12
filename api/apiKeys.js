2bc36ca85a884fba1dad142a9ef0ee2d176438a3
Stephan Lefort <lefortsge@gmail.com>
2018-01-12 14:43:31 -0800

    rebasing for styles

---
+++ api/apiKeys.js
@@ -0,0 +1,3 @@
+module.exports.NYTKey = {
+  key: "ad2386dd7c8d4de2968ee43d3310d34a"
+};
---
+++ api/goodreads.js
@@ -0,0 +1,7 @@
+module.exports.goodreadsKey = {
+
+  key: '6oijAUTgvAWaektHWXBqkQ',
+
+  secret: 'ftRnm3yWGWWO5goXiMbyBfitAgBQBSgXRFZ3gC7Q9kk'
+
+}
