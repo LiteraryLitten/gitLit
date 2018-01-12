e69e8d709e1442a36c88c273742578d0f7121702
misha <mishall.afzal@gmail.com>
2018-01-11 19:37:06 -0800

    changed title

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -20,7 +20,7 @@ class App extends React.Component {
     this.submitReview = this.submitReview.bind(this);
   }
   //
-  componentDidMount() {
+  //componentDidMount() {
     // example load user by userName
     // this.fetch('user', 'dust_off', (user) => {
     //   this.setState({
