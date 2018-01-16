f345aa53030df27eb1db1b85f3370ae99fcaa07d
misha <mishall.afzal@gmail.com>
2018-01-15 19:56:43 -0800

    SLOWLY BUT SURELY FIXING THING: getting search to search, but book cards not showing

--- react-client/src/components/SearchPage.jsx
+++ react-client/src/components/SearchPage.jsx
@@ -15,42 +15,56 @@ const styles = theme => ({
 class SearchPage extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      book: {},
-    };
-
-    this.createBook = this.createBook.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
+    this.renderPage = this.renderPage;
   }
 
   componentDidMount() {
-    let searchCards = [];
-    searchCards = this.props.searchResults.map((book) => {
-      return this.createBook(book);
-    });
-    console.log(searchCards);
-    this.setState({ searchCards: searchCards });
   }
 
-  createBook(searchResult) {
-    let book = {
-      title: searchResult.best_book.title._text,
-      author: searchResult.best_book.author.name._text,
-      imageURL: searchResult.best_book.image_url._text,
-    };
-    return book;
+  renderPage(){
+    if(this.props.searchResults) {
+      console.log(this.props.searchResults);
+      this.props.searchResults.forEach((result) => {
+        const book = {
+          title: result.best_book.title._text,
+          author: result.best_book.author.name._text,
+          imageURL: result.best_book.image_url._text,
+          description: 'NA',
+          genre: 'NA',
+        };
+        return (<BookCard book={book} />);
+      });
+      //get the necessary book data
+      //send it to BookCard
+    }
   }
 
   render() {
-
-    // console.log(this.state.searchResults);
-    console.log(this.state);
-    console.log(this.props);
-
+    const searchCards = [];
+    if(this.props.searchResults) {
+      console.log(this.props.searchResults);
+      this.props.searchResults.forEach((result) => {
+        const book = {
+          title: result.best_book.title._text,
+          author: result.best_book.author.name._text,
+          imageURL: result.best_book.image_url._text,
+          description: 'NA',
+          genre: 'NA',
+        };
+        searchCards.push(book);
+      });
+      return (
+      <div>
+        <h2>SEARCH RESULTS</h2> <br />
+        {searchCards.map((book) => {
+          <BookCard book={book} />
+        })}
+      </div>
+    );
+    }
     return (
       <div>
-      <h2>SEARCH RESULTS</h2> <br />
+        <h2>NO SEARCH RESULTS</h2> <br />
         
       </div>
     );
