5097890c046c2717a2c33862b1944171d94e9edb
Stephan Lefort <lefortsge@gmail.com>
2018-01-19 10:36:23 -0800

    In working condition

--- server/index.js
+++ server/index.js
@@ -24,28 +24,3 @@ app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
 
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const handler = require('./Handler.js');
-
-// const app = express();
-// app.use(express.static(`${__dirname}/../react-client/dist`));
-// app.use(bodyParser.json());
-
-// app.get('/user/:username', handler.getUserByName);
-// app.post('/login', handler.postLogin);
-// app.post('/signup', handler.postSignUp);
-
-// app.get('/items', handler.getAllBooks);
-// app.get('/book/:isbn', handler.getBookByISBN);
-// app.get('/search/:title', handler.getSearchTitle);
-// app.get('/bestSellers', handler.getBestSellers);
-// app.post('/review', handler.postReview);
-// app.post('/favorites', handler.postFavorites);
-// app.get('/userReviews/:isbn13', handler.getUserReviews);
-
-// app.listen(3000, () => {
-//   console.log('listening on port 3000!');
-// });
