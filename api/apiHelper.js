fdc0a0a21f0c9b7038880323912a034778d5534c
Dustin Burns <dustinburns@gmail.com>
2018-01-20 10:03:42 -0800

    Update apiHelper.js

--- api/apiHelper.js
+++ api/apiHelper.js
@@ -154,12 +154,6 @@ const getReviewsiDreams = (isbn, cb) => {
     },
   })
     .then((response) => {
-      console.log('');
-      console.log('iDream!!!');
-      console.log(response.data);
-      console.log('');
-      console.log('');
-
       cb(null, response.data);
     })
     .catch((error) => {
