a609f469a7e1a1db591c23f74528d3f4f668f21c
Dustin Burns <dustinburns@gmail.com>
2018-01-22 07:47:29 -0800

    Delete apiKeys.js

--- api/apiKeys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  NYTKey: 'ad2386dd7c8d4de2968ee43d3310d34a',
-  goodReadsKey: '6oijAUTgvAWaektHWXBqkQ',
-  goodReadsSecret: 'ftRnm3yWGWWO5goXiMbyBfitAgBQBSgXRFZ3gC7Q9kk',
-  iDreambooksKey: 'c9aaf7aed4f252efc277ba4341dc7abfaf4fd18e',
-  MONGODB_URI: process.env.MONGODB_URI,
-};
